c2d61cb2926c1a1653720b9a341984a5e4665a79
John Bowser <john.bow7720@gmail.com>
2025-12-03 20:39:46 -0500

    refactor day 3, optimize part 1

--- src/day3.ts
+++ src/day3.ts
@@ -3,27 +3,7 @@ import fs from "fs";
 const data = fs.readFileSync(`${__dirname}/../input/input_day3.txt`, "utf-8");
 const banks = data.split("\n");
 
-function part1(): string {
-  console.time("part1");
-  let totalSum = 0;
-  for (const bank of banks) {
-    const combinations: string[] = [];
-    for (let i = 0; i < bank.length; i++) {
-      for (let j = i + 1; j < bank.length; j++) {
-        combinations.push(`${bank[i]}${bank[j]}`);
-      }
-    }
-    totalSum += Math.max(...combinations.map(combination => Number(combination)));
-  }
-  console.timeEnd("part1");
-
-  return `Day 3 - Part 1: ${totalSum}`;
-}
-
-console.log(part1());
-
-function part2(): string {
-  console.time("part2");
+function getJoltage(banks: string[], numberOfDigits: number): number {
   let totalSum = 0;
   for (const bank of banks) {
     // we need to find 12 digits
@@ -32,9 +12,9 @@ function part2(): string {
     let combination = "";
     let lastSelectedDigitIndex = -1;
     // our loop will run until we have selected 12 digits.
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < numberOfDigits; i++) {
       // figure out how many digits are left to pick, and use that to define our range.
-      const digitsRemainingToPick = 12 - i;
+      const digitsRemainingToPick = numberOfDigits - i;
       let rangeStart = lastSelectedDigitIndex + 1; // this value will increase as we select digits.
       let rangeEnd = bank.length - digitsRemainingToPick; // this value will also increase, meaning our range gets closer to the end of the bank
 
@@ -56,6 +36,22 @@ function part2(): string {
 
     totalSum += Number(combination);
   }
+  return totalSum;
+}
+
+function part1(): string {
+  console.time("part1");
+  const totalSum = getJoltage(banks, 2);
+  console.timeEnd("part1");
+
+  return `Day 3 - Part 1: ${totalSum}`;
+}
+
+console.log(part1());
+
+function part2(): string {
+  console.time("part2");
+  const totalSum = getJoltage(banks, 12);
   console.timeEnd("part2");
   return `Day 3 - Part 2: ${totalSum}`;
 }
