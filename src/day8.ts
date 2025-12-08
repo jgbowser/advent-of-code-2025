916206f9c60328fa928f5ef0a082ede0da86dead
John Bowser <john.bow7720@gmail.com>
2025-12-08 15:26:29 -0500

    day 8 optimized a bit

--- src/day8.ts
+++ src/day8.ts
@@ -8,12 +8,12 @@ const points = data
   .split("\n")
   .map((line) => line.split(",").map(Number)) as Point[];
 
+const sortedDistances = findAllDistancePairs().sort((a, b) => a[0] - b[0]);
+
 function calculateDistance(p1: Point, p2: Point) {
-  return Math.sqrt(
-    Math.pow(p1[0] - p2[0], 2) +
+  return Math.pow(p1[0] - p2[0], 2) +
       Math.pow(p1[1] - p2[1], 2) +
-      Math.pow(p1[2] - p2[2], 2),
-  );
+      Math.pow(p1[2] - p2[2], 2)
 }
 
 function findAllDistancePairs(): [number, Point, Point][] {
@@ -53,9 +53,6 @@ function mergeCircuits(parentMap: Map<string, string>, point1: string, point2: s
 
 function part1(): string {
   console.time("part1");
-  const distancePairs = findAllDistancePairs();
-  // now we need to sort the results least to greatest and build our circuits
-  const sortedDistances = distancePairs.sort((a, b) => a[0] - b[0]);
   // initialize each of our points as its own unconnected circuit node (its root is itself)
   // the key is the point and the value is the root node it is connected to
   // as we make connections we update the value to point to the root of the circuit
@@ -103,9 +100,6 @@ console.log(part1());
 
 function part2(): string {
   console.time("part2");
-  const distancePairs = findAllDistancePairs();
-  const sortedDistances = distancePairs.sort((a, b) => a[0] - b[0]);
-  
   const parentMap = new Map<string, string>();
   for (const point of points) {
     const key = point.join(",");
